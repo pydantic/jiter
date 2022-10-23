4c0d7e0edae378db55e21885de856def04eeeecb
Samuel Colvin <s@muelcolvin.com>
2022-10-23 09:44:44 +0100

    improve benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -8,30 +8,27 @@ use test::{black_box, Bencher};
 use serde_json::Value;
 use donervan::{JsonValue, Chunker, Chunk};
 
-#[bench]
-fn donervan_value_pass1(bench: &mut Bencher) {
-    // pass1.json is downloaded from https://json.org/JSON_checker/test/pass1.json
-    // see https://github.com/python/cpython/blob/main/Lib/test/test_json/test_pass1.py
-    let mut f = File::open("./benches/pass1.json").unwrap();
+fn read_file(path: &str) -> String {
+    let mut file = File::open(path).unwrap();
     let mut contents = String::new();
-    f.read_to_string(&mut contents).unwrap();
+    file.read_to_string(&mut contents).unwrap();
+    contents
+}
 
-    let json_data = black_box(contents.as_bytes());
+fn donervan_value(path: &str, bench: &mut Bencher) {
+    let json = read_file(path);
+    let json_data = json.as_bytes();
     bench.iter(|| {
-        let v = JsonValue::parse(json_data).unwrap();
+        let v = JsonValue::parse(black_box(json_data)).unwrap();
         black_box(v);
     })
 }
 
-#[bench]
-fn donervan_chunker_parse_pass1(bench: &mut Bencher) {
-    let mut f = File::open("./benches/pass1.json").unwrap();
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).unwrap();
-
-    let json_data = black_box(contents.as_bytes());
+fn donervan_chunker_parse(path: &str, bench: &mut Bencher) {
+    let json = read_file(path);
+    let json_data = json.as_bytes();
     bench.iter(|| {
-        let mut chunker = Chunker::new(json_data);
+        let mut chunker = Chunker::new(black_box(json_data));
         loop {
             let chunk = match chunker.next() {
                 Some(c) => c.unwrap(),
@@ -77,13 +74,9 @@ fn donervan_chunker_parse_pass1(bench: &mut Bencher) {
     })
 }
 
-#[bench]
-fn donervan_chunker_skip_pass1(bench: &mut Bencher) {
-    let mut f = File::open("./benches/pass1.json").unwrap();
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).unwrap();
-
-    let json_data = black_box(contents.as_bytes());
+fn donervan_chunker_skip(path: &str, bench: &mut Bencher) {
+    let json = read_file(path);
+    let json_data = black_box(json.as_bytes());
     bench.iter(|| {
         let mut chunker = Chunker::new(json_data);
         loop {
@@ -107,15 +100,50 @@ fn donervan_chunker_skip_pass1(bench: &mut Bencher) {
     })
 }
 
-#[bench]
-fn serde_value_pass1(bench: &mut Bencher) {
-    let mut f = File::open("./benches/pass1.json").unwrap();
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).unwrap();
-
-    let json_data = black_box(contents.as_bytes());
+fn serde_value(path: &str, bench: &mut Bencher) {
+    let json = read_file(path);
+    let json_data = black_box(json.as_bytes());
     bench.iter(|| {
         let value: Value = serde_json::from_slice(json_data).unwrap();
         black_box(value);
     })
 }
+
+macro_rules! test_cases {
+    ($file_name:ident) => {
+        paste::item! {
+            #[bench]
+            fn [< $file_name _donervan_value >](bench: &mut Bencher) {
+                let file_path = format!("./benches/{}.json", stringify!($file_name));
+                donervan_value(&file_path, bench);
+            }
+
+            #[bench]
+            fn [< $file_name _donervan_chunker_parse >](bench: &mut Bencher) {
+                let file_path = format!("./benches/{}.json", stringify!($file_name));
+                donervan_chunker_parse(&file_path, bench);
+            }
+
+            #[bench]
+            fn [< $file_name _donervan_chunker_skip >](bench: &mut Bencher) {
+                let file_path = format!("./benches/{}.json", stringify!($file_name));
+                donervan_chunker_skip(&file_path, bench);
+            }
+
+            #[bench]
+            fn [< $file_name _serde_value >](bench: &mut Bencher) {
+                let file_path = format!("./benches/{}.json", stringify!($file_name));
+                serde_value(&file_path, bench);
+            }
+        }
+    };
+}
+
+// https://json.org/JSON_checker/test/pass1.json
+// see https://github.com/python/cpython/blob/main/Lib/test/test_json/test_pass1.py
+test_cases!(pass1);
+// https://json.org/JSON_checker/test/pass2.json
+test_cases!(pass2);
+test_cases!(string_array);
+test_cases!(true_array);
+test_cases!(true_object);
