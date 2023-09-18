7dd73d87a93bcc809ae6fe0ae80eb14060b43968
Samuel Colvin <s@muelcolvin.com>
2023-09-18 16:02:33 +0100

    linting

--- tests/test_python.rs
+++ tests/test_python.rs
@@ -5,14 +5,15 @@ use jiter::JsonValue;
 
 #[test]
 fn test_to_py_object_numeric() {
-    let value = JsonValue::parse(
-        br#"  { "int": 1, "bigint": 123456789012345678901234567890, "float": 1.2}  "#,
-    )
-    .unwrap();
+    let value =
+        JsonValue::parse(br#"  { "int": 1, "bigint": 123456789012345678901234567890, "float": 1.2}  "#).unwrap();
     Python::with_gil(|py| {
         let python_value = value.to_object(py);
         let string = python_value.as_ref(py).to_string();
-        assert_eq!(string, "{'int': 1, 'bigint': 123456789012345678901234567890, 'float': 1.2}");
+        assert_eq!(
+            string,
+            "{'int': 1, 'bigint': 123456789012345678901234567890, 'float': 1.2}"
+        );
     })
 }
 
