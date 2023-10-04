f7f20872bac794eb1608795b947eb649f798e2b4
Samuel Colvin <s@muelcolvin.com>
2023-10-04 09:29:27 +0100

    stop using _PyDict_SetItem_KnownHash

--- src/python.rs
+++ src/python.rs
@@ -1,9 +1,10 @@
+use std::cell::{RefCell, RefMut};
+
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::sync::{GILOnceCell, GILProtected};
 use pyo3::types::{PyDict, PyList, PyString};
 use pyo3::{ffi, AsPyPointer};
-use std::cell::RefCell;
 
 use ahash::AHashMap;
 use smallvec::SmallVec;
@@ -12,14 +13,14 @@ use crate::errors::{json_error, DEFAULT_RECURSION_LIMIT};
 use crate::string_decoder::Tape;
 use crate::{FilePosition, JsonError, NumberAny, NumberInt, Parser, Peak, StringDecoder};
 
-static KEYS_CACHE: GILOnceCell<GILProtected<RefCell<AHashMap<String, (PyObject, isize)>>>> = GILOnceCell::new();
+static KEYS_CACHE: GILOnceCell<GILProtected<RefCell<AHashMap<String, PyObject>>>> = GILOnceCell::new();
 
 pub fn python_parse(py: Python, data: &[u8]) -> PyResult<PyObject> {
     let protected_cache = KEYS_CACHE.get_or_init(py, || GILProtected::new(RefCell::new(AHashMap::new())));
-    let mut cache_ref_mut = protected_cache.get(py).borrow_mut();
+    let mut cache: RefMut<AHashMap<String, PyObject>> = protected_cache.get(py).borrow_mut();
 
-    if cache_ref_mut.len() > 100_000 {
-        cache_ref_mut.clear();
+    if cache.len() > 100_000 {
+        cache.clear();
     }
 
     let mut python_parser = PythonParser {
@@ -32,7 +33,7 @@ pub fn python_parse(py: Python, data: &[u8]) -> PyResult<PyObject> {
     let mje = |e: JsonError| map_json_error(data, e);
 
     let peak = python_parser.parser.peak().map_err(mje)?;
-    let v = python_parser.py_take_value(py, peak, &mut *cache_ref_mut)?;
+    let v = python_parser.py_take_value(py, peak, &mut *cache)?;
     python_parser.parser.finish().map_err(mje)?;
     Ok(v)
 }
@@ -49,7 +50,7 @@ impl<'j> PythonParser<'j> {
         &mut self,
         py: Python,
         peak: Peak,
-        keys_hashmap: &mut AHashMap<String, (PyObject, isize)>,
+        keys_hashmap: &mut AHashMap<String, PyObject>,
     ) -> PyResult<PyObject> {
         let mje = |e: JsonError| map_json_error(self.data, e);
         match peak {
@@ -97,43 +98,49 @@ impl<'j> PythonParser<'j> {
             }
             Peak::Object => {
                 let dict = PyDict::new(py);
-                let set_item = |key: PyObject, value: PyObject, hash: isize| unsafe {
-                    ffi::_PyDict_SetItem_KnownHash(dict.as_ptr(), key.as_ptr(), value.as_ptr(), hash)
+
+                let set_item = |key: PyObject, value: PyObject| {
+                    let r = unsafe { ffi::PyDict_SetItem(dict.as_ptr(), key.as_ptr(), value.as_ptr()) };
+                    // AFAIK this shouldn't happen since the key will always be a string  which is hashable
+                    // we panic here rather than returning a result and using `?` below as it's up to 14% faster
+                    // presumably because there are fewer branches
+                    if r == -1 {
+                        panic!("PyDict_SetItem failed")
+                    }
                 };
 
-                macro_rules! key_hash {
+                macro_rules! cache_key {
                     ($key:ident) => {
                         if $key.len() < 64 {
                             match keys_hashmap.get($key) {
-                                Some((key, index)) => (key.clone_ref(py), *index),
+                                Some(key) => key.clone_ref(py),
                                 None => {
-                                    let key = PyString::intern(py, $key);
-                                    let index = key.hash().unwrap_or(0);
-                                    let key_object = key.to_object(py);
-                                    keys_hashmap.insert(key.to_string(), (key_object.clone_ref(py), index));
-                                    (key_object, index)
+                                    let key_object = PyString::new(py, $key).to_object(py);
+                                    // shame we have to cache the key again here, is there way to use `entry()`
+                                    // without calling `.to_string()` in the case where the key is already cached?
+                                    keys_hashmap.insert($key.to_string(), key_object.clone_ref(py));
+                                    key_object
                                 }
                             }
                         } else {
                             let key = PyString::new(py, $key);
-                            let index = key.hash()?;
-                            (key.to_object(py), index)
+                            key.to_object(py)
                         }
                     };
                 }
 
                 if let Some(first_key) = self.parser.object_first::<StringDecoder>(&mut self.tape).map_err(mje)? {
                     let first_key_str = first_key.as_str();
-                    let (first_key, first_hash) = key_hash!(first_key_str);
+                    let first_key = key_hash!(first_key_str);
                     let peak = self.parser.peak().map_err(mje)?;
                     let first_value = self._check_take_value(py, peak, keys_hashmap)?;
-                    set_item(first_key, first_value, first_hash);
+                    set_item(first_key, first_value);
                     while let Some(key) = self.parser.object_step::<StringDecoder>(&mut self.tape).map_err(mje)? {
                         let key_str = key.as_str();
-                        let (key, hash) = key_hash!(key_str);
+                        let key = key_hash!(key_str);
                         let peak = self.parser.peak().map_err(mje)?;
                         let value = self._check_take_value(py, peak, keys_hashmap)?;
-                        set_item(key, value, hash);
+                        set_item(key, value);
                     }
                 }
                 Ok(dict.to_object(py))
@@ -145,7 +152,7 @@ impl<'j> PythonParser<'j> {
         &mut self,
         py: Python,
         peak: Peak,
-        keys_hashmap: &mut AHashMap<String, (PyObject, isize)>,
+        keys_hashmap: &mut AHashMap<String, PyObject>,
     ) -> PyResult<PyObject> {
         self.recursion_limit = match self.recursion_limit.checked_sub(1) {
             Some(limit) => limit,
