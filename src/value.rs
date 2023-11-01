4c57761fec01eb1ff49a2b4a95ebdd1d6ba3602f
Samuel Colvin <s@muelcolvin.com>
2023-11-01 18:30:30 +0000

    adding index to JsonValue

--- src/value.rs
+++ src/value.rs
@@ -11,7 +11,13 @@ use crate::string_decoder::{StringDecoder, Tape};
 use crate::JsonError;
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum JsonValue {
+pub struct JsonValue {
+    pub index: usize,
+    pub data: JsonData,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum JsonData {
     Null,
     Bool(bool),
     Int(i64),
@@ -27,15 +33,15 @@ pub type JsonObject = Arc<LazyIndexMap<String, JsonValue>>;
 #[cfg(feature = "python")]
 impl pyo3::ToPyObject for JsonValue {
     fn to_object(&self, py: pyo3::Python<'_>) -> pyo3::PyObject {
-        match self {
-            Self::Null => py.None(),
-            Self::Bool(b) => b.to_object(py),
-            Self::Int(i) => i.to_object(py),
-            Self::BigInt(b) => b.to_object(py),
-            Self::Float(f) => f.to_object(py),
-            Self::String(s) => s.to_object(py),
-            Self::Array(v) => pyo3::types::PyList::new(py, v.iter().map(|v| v.to_object(py))).to_object(py),
-            Self::Object(o) => {
+        match &self.data {
+            JsonData::Null => py.None(),
+            JsonData::Bool(b) => b.to_object(py),
+            JsonData::Int(i) => i.to_object(py),
+            JsonData::BigInt(b) => b.to_object(py),
+            JsonData::Float(f) => f.to_object(py),
+            JsonData::String(s) => s.to_object(py),
+            JsonData::Array(v) => pyo3::types::PyList::new(py, v.iter().map(|v| v.to_object(py))).to_object(py),
+            JsonData::Object(o) => {
                 let dict = pyo3::types::PyDict::new(py);
                 for (k, v) in o.iter() {
                     dict.set_item(k, v.to_object(py)).unwrap();
@@ -61,6 +67,10 @@ impl JsonValue {
         parser.finish().map_err(map_err)?;
         Ok(v)
     }
+
+    pub fn new(index: usize, data: JsonData) -> Self {
+        Self { index, data }
+    }
 }
 
 macro_rules! check_recursion {
@@ -82,29 +92,30 @@ pub(crate) fn take_value(
     tape: &mut Tape,
     mut recursion_limit: u8,
 ) -> JsonResult<JsonValue> {
+    let index = parser.index;
     match peak {
         Peak::True => {
             parser.consume_true()?;
-            Ok(JsonValue::Bool(true))
+            Ok(JsonValue::new(index, JsonData::Bool(true)))
         }
         Peak::False => {
             parser.consume_false()?;
-            Ok(JsonValue::Bool(false))
+            Ok(JsonValue::new(index, JsonData::Bool(false)))
         }
         Peak::Null => {
             parser.consume_null()?;
-            Ok(JsonValue::Null)
+            Ok(JsonValue::new(index, JsonData::Null))
         }
         Peak::String => {
             let s = parser.consume_string::<StringDecoder>(tape)?;
-            Ok(JsonValue::String(s.to_string()))
+            Ok(JsonValue::new(index, JsonData::String(s.to_string())))
         }
         Peak::Num(first) => {
             let n = parser.consume_number::<NumberAny>(first)?;
             match n {
-                NumberAny::Int(NumberInt::Int(int)) => Ok(JsonValue::Int(int)),
-                NumberAny::Int(NumberInt::BigInt(big_int)) => Ok(JsonValue::BigInt(big_int)),
-                NumberAny::Float(float) => Ok(JsonValue::Float(float)),
+                NumberAny::Int(NumberInt::Int(int)) => Ok(JsonValue::new(index, JsonData::Int(int))),
+                NumberAny::Int(NumberInt::BigInt(big_int)) => Ok(JsonValue::new(index, JsonData::BigInt(big_int))),
+                NumberAny::Float(float) => Ok(JsonValue::new(index, JsonData::Float(float))),
             }
         }
         Peak::Array => {
@@ -122,7 +133,7 @@ pub(crate) fn take_value(
                     array.push(v);
                 }
             }
-            Ok(JsonValue::Array(Arc::new(array)))
+            Ok(JsonValue::new(index, JsonData::Array(Arc::new(array))))
         }
         Peak::Object => {
             // same for objects
@@ -144,7 +155,7 @@ pub(crate) fn take_value(
                 }
             }
 
-            Ok(JsonValue::Object(Arc::new(object)))
+            Ok(JsonValue::new(index, JsonData::Object(Arc::new(object))))
         }
     }
 }
