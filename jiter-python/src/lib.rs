a67df867a458038f77910386464c3d7e074a359e
David Hewitt <mail@davidhewitt.dev>
2023-11-20 15:17:05 +0000

    update to PyO3 0.21.0-beta.0

--- jiter-python/src/lib.rs
+++ jiter-python/src/lib.rs
@@ -1,9 +1,10 @@
 use pyo3::prelude::*;
+use pyo3::pymodule;
 
 use jiter::{python_parse, map_json_error};
 
 #[pyfunction(signature = (data, *, allow_inf_nan=true, cache_strings=true))]
-pub fn from_json(py: Python, data: &[u8], allow_inf_nan: bool, cache_strings: bool) -> PyResult<PyObject> {
+pub fn from_json<'py>(py: Python<'py>, data: &'py [u8], allow_inf_nan: bool, cache_strings: bool) -> PyResult<Bound<'py, PyAny>> {
     let json_bytes = data;
     python_parse(py, json_bytes, allow_inf_nan, cache_strings).map_err(|e| map_json_error(json_bytes, &e))
 }
