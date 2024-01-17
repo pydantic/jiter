1ce40b322ae29fb4788ba03b476a0e72cb9ce3ef
Samuel Colvin <s@muelcolvin.com>
2024-01-17 10:39:20 +0000

    fix with python parsing seen on pydantic-core (#54)

--- src/python.rs
+++ src/python.rs
@@ -9,7 +9,7 @@ use pyo3::{ffi, AsPyPointer};
 use hashbrown::hash_map::{HashMap, RawEntryMut};
 use smallvec::SmallVec;
 
-use crate::errors::{json_err, JsonError, JsonResult, DEFAULT_RECURSION_LIMIT};
+use crate::errors::{json_err, json_error, JsonError, JsonResult, DEFAULT_RECURSION_LIMIT};
 use crate::number_decoder::{NumberAny, NumberInt};
 use crate::parse::{Parser, Peek};
 use crate::string_decoder::{StringDecoder, Tape};
@@ -59,6 +59,10 @@ struct PythonParser<'j> {
 impl<'j> PythonParser<'j> {
     fn py_take_value<StringCache: StringMaybeCache>(&mut self, py: Python, peek: Peek) -> JsonResult<PyObject> {
         match peek {
+            Peek::Null => {
+                self.parser.consume_null()?;
+                Ok(py.None())
+            }
             Peek::True => {
                 self.parser.consume_true()?;
                 Ok(true.to_object(py))
@@ -66,10 +70,6 @@ impl<'j> PythonParser<'j> {
             Peek::False => {
                 self.parser.consume_false()?;
                 Ok(false.to_object(py))
-            }
-            Peek::Null => {
-                self.parser.consume_null()?;
-                Ok(py.None())
             }
             Peek::String => {
                 let s = self.parser.consume_string::<StringDecoder>(&mut self.tape)?;
@@ -120,11 +120,18 @@ impl<'j> PythonParser<'j> {
             _ => {
                 let n = self
                     .parser
-                    .consume_number::<NumberAny>(peek.into_inner(), self.allow_inf_nan)?;
+                    .consume_number::<NumberAny>(peek.into_inner(), self.allow_inf_nan);
                 match n {
-                    NumberAny::Int(NumberInt::Int(int)) => Ok(int.to_object(py)),
-                    NumberAny::Int(NumberInt::BigInt(big_int)) => Ok(big_int.to_object(py)),
-                    NumberAny::Float(float) => Ok(float.to_object(py)),
+                    Ok(NumberAny::Int(NumberInt::Int(int))) => Ok(int.to_object(py)),
+                    Ok(NumberAny::Int(NumberInt::BigInt(big_int))) => Ok(big_int.to_object(py)),
+                    Ok(NumberAny::Float(float)) => Ok(float.to_object(py)),
+                    Err(e) => {
+                        if !peek.is_num() {
+                            Err(json_error!(ExpectedSomeValue, self.parser.index))
+                        } else {
+                            Err(e)
+                        }
+                    }
                 }
             }
         }
