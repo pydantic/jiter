13070b94e9c24314fe7347fe6f6a6f6d5f468334
Samuel Colvin <s@muelcolvin.com>
2024-01-20 14:49:44 +0000

    python package for benchmarking (#60)

---
+++ jiter-python/bench.py
@@ -0,0 +1,48 @@
+import timeit
+from pathlib import Path
+
+import orjson
+import jiter_python
+import ujson
+import json
+
+cases = [
+    ('medium_response', Path('../benches/medium_response.json').read_bytes()),
+    ('massive_ints_array', Path('../benches/massive_ints_array.json').read_bytes()),
+    ('array_short_strings', '[{}]'.format(', '.join('"123"' for _ in range(100_000)))),
+    ('object_short_strings', '{%s}' % ', '.join(f'"{i}": "{i}x"' for i in range(100_000))),
+    ('array_short_arrays', '[{}]'.format(', '.join('["a", "b", "c", "d"]' for _ in range(10_000)))),
+    ('one_long_string', json.dumps('x' * 100)),
+    ('one_short_string', b'"foobar"'),
+]
+
+
+def run_bench(func, d):
+    if isinstance(d, str):
+        d = d.encode()
+    timer = timeit.Timer(
+        'func(json_data)', setup='', globals={'func': func, 'json_data': d}
+    )
+    n, t = timer.autorange()
+    iter_time = t / n
+    # print(f'{func.__module__}.{func.__name__}', iter_time)
+    return iter_time
+
+
+for name, json_data in cases:
+    print(f'Case: {name}')
+    times = [
+        ('orjson', run_bench(orjson.loads, json_data)),
+        ('jiter', run_bench(jiter_python.from_json, json_data)),
+        ('ujson', run_bench(ujson.loads, json_data)),
+        ('json', run_bench(json.loads, json_data)),
+    ]
+
+    times.sort(key=lambda x: x[1])
+    best = times[0][1]
+
+    print(f'{"package":>12} | {"time µs":>10} | slowdown')
+    print(f'{"-" * 13}|{"-" * 12}|{"-" * 9}')
+    for name, time in times:
+        print(f'{name:>12} | {time * 1_000_000:10.2f} | {time / best:8.2f}')
+    print('')
---
+++ jiter-python/test.py
@@ -0,0 +1,6 @@
+import jiter_python
+
+if __name__ == '__main__':
+    assert jiter_python.from_json(b'[true, false, null, 123, 456.7]') == [True, False, None, 123, 456.7]
+    assert jiter_python.from_json(b'{"foo": "bar"}') == {'foo': 'bar'}
+    print('tested passed')
