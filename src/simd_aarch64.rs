79de565f5d916970a3e6e0a6f04405fe19590a67
Samuel Colvin <s@muelcolvin.com>
2024-01-26 07:23:34 +0000

    simplify somewhat

--- src/simd_aarch64.rs
+++ src/simd_aarch64.rs
@@ -73,136 +73,117 @@ const ALT_MUL_U32_4: SimdVecu32_4 = simd_const!([10000u32, 1u32, 10000u32, 1u32]
 
 #[inline(always)]
 pub(crate) fn decode_int_chunk(data: &[u8], index: usize) -> (IntChunk, usize) {
-    #[rustfmt::skip]
-    fn get_digit_mask(byte_vec: SimdVecu8_16) -> SimdVecu8_16 {
-        unsafe {
-            simd_or_16(
-                simd_lt_16(byte_vec, ZERO_DIGIT_16),
-                simd_gt_16(byte_vec, NINE_DIGIT_16),
-            )
-        }
-    }
-
-    unsafe fn first_half_calc(byte_vec: SimdVecu8_16, last_digit: u32) -> u64 {
-        let small_byte_vec = simd_get_low(byte_vec);
-        // subtract ascii '0' from every byte to get the digit values
-        let digits: SimdVecu8_8 = simd_sub_8(small_byte_vec, ZERO_DIGIT_U8_8);
-        let digits = match last_digit {
-            0 => return 0,
-            1 => {
-                let t: [u8; 8] = transmute(digits);
-                return t[0] as u64;
-            }
-            2 => combine_vecs_8::<2>(ZERO_VAL_U8_8, digits),
-            3 => combine_vecs_8::<3>(ZERO_VAL_U8_8, digits),
-            4 => combine_vecs_8::<4>(ZERO_VAL_U8_8, digits),
-            5 => combine_vecs_8::<5>(ZERO_VAL_U8_8, digits),
-            6 => combine_vecs_8::<6>(ZERO_VAL_U8_8, digits),
-            7 => combine_vecs_8::<7>(ZERO_VAL_U8_8, digits),
-            8 => digits,
-            _ => unreachable!("last_digit should be less than 8"),
-        };
-        // multiple every other digit by 10
-        let x: SimdVecu8_8 = simd_mul_8(digits, ALT_MUL_U8_8);
-        // add the value together and combine the 8x8-bit lanes into 4x16-bit lanes
-        let x: SimdVecu16_4 = simd_add_8(x);
-        // multiple every other digit by 100
-        let x: SimdVecu16_4 = simd_mul_u16_4(x, ALT_MUL_U16_4);
-        // add the value together and combine the 4x16-bit lanes into 2x32-bit lanes
-        let x: SimdVecu32_2 = simd_add_u16_4(x);
-        // multiple the first value 10000
-        let x: SimdVecu32_2 = simd_mul_u32_2(x, ALT_MUL_U32_2);
-        // add the value together and combine the 2x32-bit lanes into 1x64-bit lane
-        let x: SimdVecu64_1 = simd_add_u32_2(x);
-        // transmute the 64-bit lane into a u64
-        transmute(x)
-    }
-
-    unsafe fn full_calc(byte_vec: SimdVecu8_16, last_digit: u32) -> u64 {
-        // subtract ascii '0' from every byte to get the digit values
-        let digits: SimdVecu8_16 = simd_sub_16(byte_vec, ZERO_DIGIT_16);
-        let digits = match last_digit {
-            9 => combine_vecs_16::<9>(ZERO_VAL_U8_16, digits),
-            10 => combine_vecs_16::<10>(ZERO_VAL_U8_16, digits),
-            11 => combine_vecs_16::<11>(ZERO_VAL_U8_16, digits),
-            12 => combine_vecs_16::<12>(ZERO_VAL_U8_16, digits),
-            13 => combine_vecs_16::<13>(ZERO_VAL_U8_16, digits),
-            14 => combine_vecs_16::<14>(ZERO_VAL_U8_16, digits),
-            15 => combine_vecs_16::<15>(ZERO_VAL_U8_16, digits),
-            16 => digits,
-            _ => unreachable!("last_digit should be between 9 and 16"),
-        };
-        // multiple every other digit by 10
-        let x: SimdVecu8_16 = simd_mul_16(digits, ALT_MUL_U8_16);
-        // add the value together and combine the 16x8-bit lanes into 8x16-bit lanes
-        let x: SimdVecu16_8 = simd_add_16(x);
-        // multiple every other digit by 100
-        let x: SimdVecu16_8 = simd_mul_u16_8(x, ALT_MUL_U16_8);
-        // add the value together and combine the 8x16-bit lanes into 4x32-bit lanes
-        let x: SimdVecu32_4 = simd_add_u16_8(x);
-        // multiple every other digit by 10000
-        let x: SimdVecu32_4 = simd_mul_u32_4(x, ALT_MUL_U32_4);
-        // add the value together and combine the 4x32-bit lanes into 2x64-bit lane
-        let x: SimdVecu64_2 = simd_add_u32_4(x);
-
-        // transmute the 2x64-bit lane into an array;
-        let t: [u64; 2] = transmute(x);
-        // since the data started out as digits, it's safe to assume the result fits in a u64
-        t[0].wrapping_mul(100000000).wrapping_add(t[1])
-    }
-
-    fn next_is_float(data: &[u8], index: usize) -> bool {
-        let next = unsafe { data.get_unchecked(index) };
-        matches!(next, b'.' | b'e' | b'E')
-    }
-
     if let Some(byte_chunk) = data.get(index..index + SIMD_STEP) {
         let byte_vec = load_slice(byte_chunk);
 
         let digit_mask = get_digit_mask(byte_vec);
-        return if is_zero(digit_mask) {
+        if is_zero(digit_mask) {
             // all lanes are digits, parse the full vector
             let value = unsafe { full_calc(byte_vec, 16) };
             (IntChunk::Ongoing(value), index + SIMD_STEP)
         } else {
             // some lanes are not digits, transmute to a pair of u64 and find the first non-digit
-            let t: [u64; 2] = unsafe { transmute(digit_mask) };
-            if t[0] != 0 {
+            let last_digit = find_end(digit_mask);
+            let index = index + last_digit as usize;
+            if next_is_float(data, index) {
+                (IntChunk::Float, index)
+            } else if last_digit <= 8 {
                 // none-digit in the first 8 bytes
-                let last_digit = t[0].trailing_zeros() / 8;
-                let index = index + last_digit as usize;
-                if next_is_float(data, index) {
-                    (IntChunk::Float, index)
-                } else {
-                    let value = unsafe { first_half_calc(byte_vec, last_digit) };
-                    (IntChunk::Done(value), index)
-                }
+                let value = unsafe { first_half_calc(byte_vec, last_digit) };
+                (IntChunk::Done(value), index)
             } else {
                 // none-digit in the last 8 bytes
-                let last_digit = t[1].trailing_zeros() / 8 + 8;
-                if last_digit == 8 {
-                    // all bytes in the first half are digits
-                    let index = index + 8;
-                    if next_is_float(data, index) {
-                        (IntChunk::Float, index)
-                    } else {
-                        let value = unsafe { first_half_calc(byte_vec, 8) };
-                        (IntChunk::Done(value), index)
-                    }
-                } else {
-                    let index = index + last_digit as usize;
-                    if next_is_float(data, index) {
-                        (IntChunk::Float, index)
-                    } else {
-                        let value = unsafe { full_calc(byte_vec, last_digit) };
-                        (IntChunk::Done(value), index)
-                    }
-                }
+                let value = unsafe { full_calc(byte_vec, last_digit) };
+                (IntChunk::Done(value), index)
             }
-        };
+        }
+    } else {
+        // we got near the end of the string, fall back to the slow path
+        decode_int_chunk_fallback(data, index)
     }
-    // we got near the end of the string, fall back to the slow path
-    decode_int_chunk_fallback(data, index)
+}
+
+#[rustfmt::skip]
+fn get_digit_mask(byte_vec: SimdVecu8_16) -> SimdVecu8_16 {
+    unsafe {
+        simd_or_16(
+            simd_lt_16(byte_vec, ZERO_DIGIT_16),
+            simd_gt_16(byte_vec, NINE_DIGIT_16),
+        )
+    }
+}
+
+unsafe fn first_half_calc(byte_vec: SimdVecu8_16, last_digit: u32) -> u64 {
+    let small_byte_vec = simd_get_low(byte_vec);
+    // subtract ascii '0' from every byte to get the digit values
+    let digits: SimdVecu8_8 = simd_sub_8(small_byte_vec, ZERO_DIGIT_U8_8);
+    let digits = match last_digit {
+        0 => return 0,
+        1 => {
+            let t: [u8; 8] = transmute(digits);
+            return t[0] as u64;
+        }
+        2 => combine_vecs_8::<2>(ZERO_VAL_U8_8, digits),
+        3 => combine_vecs_8::<3>(ZERO_VAL_U8_8, digits),
+        4 => combine_vecs_8::<4>(ZERO_VAL_U8_8, digits),
+        5 => combine_vecs_8::<5>(ZERO_VAL_U8_8, digits),
+        6 => combine_vecs_8::<6>(ZERO_VAL_U8_8, digits),
+        7 => combine_vecs_8::<7>(ZERO_VAL_U8_8, digits),
+        8 => digits,
+        _ => unreachable!("last_digit should be less than 8"),
+    };
+    // multiple every other digit by 10
+    let x: SimdVecu8_8 = simd_mul_8(digits, ALT_MUL_U8_8);
+    // add the value together and combine the 8x8-bit lanes into 4x16-bit lanes
+    let x: SimdVecu16_4 = simd_add_8(x);
+    // multiple every other digit by 100
+    let x: SimdVecu16_4 = simd_mul_u16_4(x, ALT_MUL_U16_4);
+    // add the value together and combine the 4x16-bit lanes into 2x32-bit lanes
+    let x: SimdVecu32_2 = simd_add_u16_4(x);
+    // multiple the first value 10000
+    let x: SimdVecu32_2 = simd_mul_u32_2(x, ALT_MUL_U32_2);
+    // add the value together and combine the 2x32-bit lanes into 1x64-bit lane
+    let x: SimdVecu64_1 = simd_add_u32_2(x);
+    // transmute the 64-bit lane into a u64
+    transmute(x)
+}
+
+unsafe fn full_calc(byte_vec: SimdVecu8_16, last_digit: u32) -> u64 {
+    // subtract ascii '0' from every byte to get the digit values
+    let digits: SimdVecu8_16 = simd_sub_16(byte_vec, ZERO_DIGIT_16);
+    let digits = match last_digit {
+        9 => combine_vecs_16::<9>(ZERO_VAL_U8_16, digits),
+        10 => combine_vecs_16::<10>(ZERO_VAL_U8_16, digits),
+        11 => combine_vecs_16::<11>(ZERO_VAL_U8_16, digits),
+        12 => combine_vecs_16::<12>(ZERO_VAL_U8_16, digits),
+        13 => combine_vecs_16::<13>(ZERO_VAL_U8_16, digits),
+        14 => combine_vecs_16::<14>(ZERO_VAL_U8_16, digits),
+        15 => combine_vecs_16::<15>(ZERO_VAL_U8_16, digits),
+        16 => digits,
+        _ => unreachable!("last_digit should be between 9 and 16"),
+    };
+    // multiple every other digit by 10
+    let x: SimdVecu8_16 = simd_mul_16(digits, ALT_MUL_U8_16);
+    // add the value together and combine the 16x8-bit lanes into 8x16-bit lanes
+    let x: SimdVecu16_8 = simd_add_16(x);
+    // multiple every other digit by 100
+    let x: SimdVecu16_8 = simd_mul_u16_8(x, ALT_MUL_U16_8);
+    // add the value together and combine the 8x16-bit lanes into 4x32-bit lanes
+    let x: SimdVecu32_4 = simd_add_u16_8(x);
+    // multiple every other digit by 10000
+    let x: SimdVecu32_4 = simd_mul_u32_4(x, ALT_MUL_U32_4);
+    // add the value together and combine the 4x32-bit lanes into 2x64-bit lane
+    let x: SimdVecu64_2 = simd_add_u32_4(x);
+
+    // transmute the 2x64-bit lane into an array;
+    let t: [u64; 2] = transmute(x);
+    // since the data started out as digits, it's safe to assume the result fits in a u64
+    t[0].wrapping_mul(100000000).wrapping_add(t[1])
+}
+
+fn next_is_float(data: &[u8], index: usize) -> bool {
+    let next = unsafe { data.get_unchecked(index) };
+    matches!(next, b'.' | b'e' | b'E')
 }
 
 const JSON_MIN_16: SimdVecu8_16 = simd_const!([32u8; 16]);
@@ -216,20 +197,6 @@ pub fn decode_string_chunk(
     mut index: usize,
     mut ascii_only: bool,
 ) -> JsonResult<(StringChunk, bool, usize)> {
-    #[rustfmt::skip]
-    /// returns a mask where any non-zero byte means we should parsing a JSON string
-    fn json_stop_mask(byte_vec: SimdVecu8_16) -> SimdVecu8_16 {
-        unsafe {
-            simd_or_16(
-                simd_eq_16(byte_vec, QUOTE_16),
-                simd_or_16(
-                    simd_eq_16(byte_vec, BACKSLASH_16),
-                    simd_lt_16(byte_vec, JSON_MIN_16),
-                )
-            )
-        }
-    }
-
     let chunks = data
         .get(index..)
         .into_iter()
@@ -249,14 +216,7 @@ pub fn decode_string_chunk(
             continue;
         }
         // some lane(s) are a stop character, means we have to stop and find the first non-zero byte
-        let t: [u64; 2] = unsafe { transmute(stop_mask) };
-        let stop_index = if t[0] != 0 {
-            // stop char in the first 8 bytes
-            (t[0].trailing_zeros() / 8) as usize
-        } else {
-            // stop char in the second 8 bytes
-            (t[1].trailing_zeros() / 8 + 8) as usize
-        };
+        let stop_index = find_end(stop_mask) as usize;
         if ascii_only {
             // we need to mask out the bit after the last char, then check if there are any non-ascii bytes
             let stop_mask = unsafe { STOP_MASKS.get_unchecked(stop_index) };
@@ -270,6 +230,20 @@ pub fn decode_string_chunk(
     }
     // we got near the end of the string, fall back to the slow path
     StringChunk::decode_fallback(data, index, ascii_only)
+}
+
+#[rustfmt::skip]
+/// returns a mask where any non-zero byte means we should stop parsing a JSON string
+fn json_stop_mask(byte_vec: SimdVecu8_16) -> SimdVecu8_16 {
+    unsafe {
+        simd_or_16(
+            simd_eq_16(byte_vec, QUOTE_16),
+            simd_or_16(
+                simd_eq_16(byte_vec, BACKSLASH_16),
+                simd_lt_16(byte_vec, JSON_MIN_16),
+            )
+        )
+    }
 }
 
 /// mask for each position of the stop char
@@ -295,6 +269,16 @@ const STOP_MASKS: [SimdVecu8_16; 16] = {
         simd_const!([XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, __]),
     ]
 };
+
+fn find_end(digit_mask: SimdVecu8_16) -> u32 {
+    let t: [u64; 2] = unsafe { transmute(digit_mask) };
+    if t[0] != 0 {
+        // none-digit in the first 8 bytes
+        t[0].trailing_zeros() / 8
+    } else {
+        t[1].trailing_zeros() / 8 + 8
+    }
+}
 
 /// return true if all bytes are zero
 fn is_zero(vec: SimdVecu8_16) -> bool {
