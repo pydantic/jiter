37f9138de33d043fd85cf111e6afc51d4b2e7f5c
Samuel Colvin <s@muelcolvin.com>
2024-04-02 10:12:03 +0100

    fast path for ASCII python strings (#72)
    Co-authored-by: David Hewitt <david.hewitt@pydantic.dev>

--- src/string_decoder.rs
+++ src/string_decoder.rs
@@ -25,15 +25,15 @@ pub enum StringOutput<'t, 'j>
 where
     'j: 't,
 {
-    Tape(&'t str),
-    Data(&'j str),
+    Tape(&'t str, bool),
+    Data(&'j str, bool),
 }
 
 impl From<StringOutput<'_, '_>> for String {
     fn from(val: StringOutput) -> Self {
         match val {
-            StringOutput::Tape(s) => s.to_owned(),
-            StringOutput::Data(s) => s.to_owned(),
+            StringOutput::Tape(s, _) => s.to_owned(),
+            StringOutput::Data(s, _) => s.to_owned(),
         }
     }
 }
@@ -41,8 +41,8 @@ impl From<StringOutput<'_, '_>> for String {
 impl<'t, 'j> From<StringOutput<'t, 'j>> for Cow<'j, str> {
     fn from(val: StringOutput<'t, 'j>) -> Self {
         match val {
-            StringOutput::Tape(s) => s.to_owned().into(),
-            StringOutput::Data(s) => s.into(),
+            StringOutput::Tape(s, _) => s.to_owned().into(),
+            StringOutput::Data(s, _) => s.into(),
         }
     }
 }
@@ -50,8 +50,15 @@ impl<'t, 'j> From<StringOutput<'t, 'j>> for Cow<'j, str> {
 impl<'t, 'j> StringOutput<'t, 'j> {
     pub fn as_str(&self) -> &'t str {
         match self {
-            Self::Tape(s) => s,
-            Self::Data(s) => s,
+            Self::Tape(s, _) => s,
+            Self::Data(s, _) => s,
+        }
+    }
+
+    pub fn ascii_only(&self) -> bool {
+        match self {
+            Self::Tape(_, ascii_only) => *ascii_only,
+            Self::Data(_, ascii_only) => *ascii_only,
         }
     }
 }
@@ -143,7 +150,7 @@ where
                     CharType::Quote => {
                         let s = to_str(&data[start..index], ascii_only, start)?;
                         index += 1;
-                        return Ok((StringOutput::Data(s), index));
+                        return Ok((StringOutput::Data(s, ascii_only), index));
                     }
                     CharType::Backslash => return decode_to_tape(data, index, tape, start, ascii_only),
                     CharType::ControlChar => return json_err!(ControlCharacterWhileParsingString, index),
@@ -204,7 +211,7 @@ fn decode_to_tape<'t, 'j>(
                     tape.extend_from_slice(&data[last_escape..index]);
                     index += 1;
                     let s = to_str(tape, ascii_only, start)?;
-                    return Ok((StringOutput::Tape(s), index));
+                    return Ok((StringOutput::Tape(s, ascii_only), index));
                 }
                 CharType::Backslash => on_backslash!(),
                 CharType::ControlChar => return json_err!(ControlCharacterWhileParsingString, index),
